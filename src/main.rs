97359fb6d429a4adc4fb3fe43b8395061d159682
dmcong <congduong@descartes.network>
2024-03-01 10:10:31 +0700

    fix port

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ async fn sum(params: web::Path<Params>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(health).service(sum))
-        .bind(("0.0.0.0", 9999))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
